f92a4ef65176098830914449833031a7ed86858a
ShaarDW <juanthiagoalfaro@gmail.com>
2025-10-27 17:28:02 -0300

    Change HTML language attribute to Spanish and disable debug mode in game configuration

--- src/game/main.js
+++ src/game/main.js
@@ -29,7 +29,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 1000 },
-            debug: true
+            debug: false
         }
     },
     input: {
